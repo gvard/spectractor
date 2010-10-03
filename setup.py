014de55877f03063a9b104d43745006f9e1e32a4
gvardovich <gvardovich@users.sourceforge.net>
2010-10-03 16:41:21 +0400

    Add a C extension with functions for writing fds and bin files. Use numpy here.

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from distutils.core import setup, Extension
+from numpy import get_include
+
+spext = Extension('_spectractor',
+    define_macros = [('MAJOR_VERSION', '0'),
+                     ('MINOR_VERSION', '1')],
+    include_dirs = [get_include()],
+    libraries = ['m'],
+    extra_compile_args = ["-O", "-march=native"], # '-Wall', '-g'
+    sources = [os.path.join('src', 'spectractor.c')])
 
 description = """Spectractor is a library for manipulating one-dimentional
 astronomical spectra, containing number of functions in pure python.
@@ -13,5 +23,6 @@ setup(name='spectractor',
     author_email='gvardopo4ta@gmail.com',
     url='http://spectractor.sourceforge.net/',
     py_modules=['spectractor'],
+    ext_modules = [spext]
     )
 
